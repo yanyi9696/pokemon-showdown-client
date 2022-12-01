e94ddf188eef89c6b3c8d60fb1135774aaf1d0ec
Nihilslave <ApclpsZm90@hotmail.com>
2022-12-01 23:32:26 +0800

    bc - fix typo

--- config/config.js
+++ config/config.js
@@ -172,7 +172,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (b4ad0aa3/b5bf36f3)";
+Config.version = "0.11.2 (3d8ab191/b5bf36f3)";
 
 Config.routes = {
 	root: 'ps.sciroccogti.top',
