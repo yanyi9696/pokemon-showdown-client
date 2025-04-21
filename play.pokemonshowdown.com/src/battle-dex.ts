8c7726a0a7663020b05e298255d67b476124c639
Anglos <2448172102@qq.com>
2025-04-21 23:28:21 +0800

    split('-')[0];改为split('·')[0];

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -877,7 +877,7 @@ export const Dex = new class implements ModdedDex {
 			// spriteid = gen9fantasySpecies.spriteid;
 			// if (spriteid === ...) {}
 			// else {}
-			spriteid = gen9fantasySpecies.spriteid.split('-')[0];if (gen9fantasySpecies.spriteid.split('-')[1]==='mega') spriteid += '-mega';
+			spriteid = gen9fantasySpecies.spriteid.split('·')[0];
 		}
 		const spriteData: TeambuilderSpriteData = {
 			spriteid,
