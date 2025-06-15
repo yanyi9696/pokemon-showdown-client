3f0926809e19062c5f68f5b625af1ef39460f782
Fr4nK2hN <729046112@qq.com>
2025-06-15 16:38:32 +0800

    更新服务器以及git设置

--- config/config-example.js
+++ config/config-example.js
@@ -19,9 +19,9 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'sim3.psim.us',
+	host: 'server.psfantasy.xyz',
 	port: 443,
-	httpport: 8000,
+	httpport: 80,
 	altport: 80,
 	registered: true
 };
