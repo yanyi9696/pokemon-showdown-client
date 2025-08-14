4fd2475ac24a31941ffe53c5a207e540095ee9dc
Fr4nK2hN <729046112@qq.com>
2025-08-14 12:34:50 +0800

    继续修改

--- play.pokemonshowdown.com/js/replay-embed.template.js
+++ play.pokemonshowdown.com/js/replay-embed.template.js
@@ -47,8 +47,8 @@ requireScript('https://play.psfantasy.xyz/data/moves.js?a7');
 requireScript('https://play.psfantasy.xyz/data/abilities.js?a7');
 requireScript('https://play.psfantasy.xyz/data/items.js?a7');
 requireScript('https://play.psfantasy.xyz/data/teambuilder-tables.js?a7');
-requireScript('https://play.psfantasy.xyz/js/battle-tooltips.js?a7');
-requireScript('https://play.psfantasy.xyz/js/battle.js?a7');
+requireScript('https://play.pokemonshowdown.com/js/battle-tooltips.js?a7');
+requireScript('https://play.pokemonshowdown.com/js/battle.js?a7');
 
 var Replays = {
 	battle: null,
