067fb141a6f29ff8c29f98bd94bd87863ab67fe0
Anglos <2448172102@qq.com>
2025-09-06 20:38:27 +0800

    顶置飞水手里剑

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1356,6 +1356,7 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 			case 'punishment':
 			case 'zhukaibo':
 			case 'juenianpo':
+			case 'watershuriken':
 			case 'huangjinjibanshoulijian':
 				return true;
 			return false;
