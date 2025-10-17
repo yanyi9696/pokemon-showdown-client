561c7d818e8626a3fc3b3c064dd982cf60879914
Anglos <2448172102@qq.com>
2025-10-17 15:11:24 +0800

    制作幻想mega雪妖女

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -525,6 +525,10 @@ export const Dex = new class implements ModdedDex {
 			if (!data) data = {}; // 确保 data 对象存在，以防万一
 			data.spriteid = 'chandelure';
 		}
+		if (id === 'froslassmegafantasy') {
+			if (!data) data = {}; // 确保 data 对象存在，以防万一
+			data.spriteid = 'froslass';
+		}
 		// ===== 我们添加的强制贴图修正代码 结束 =====
 
 			let species: Species;
@@ -1851,7 +1855,7 @@ const ModModifier: {
 		},
 		ModifyTierSet: (tierSet: SearchRow[], dex: ModdedDex, extra?: any): SearchRow[] => {
 			// 【新代码】创建一个“钉选”白名单
-			const pinnedPokemon = ['victreebelmegafantasy', 'hawluchamegafantasy', 'chandeluremegafantasy'];
+			const pinnedPokemon = ['victreebelmegafantasy', 'hawluchamegafantasy', 'chandeluremegafantasy', 'froslassmegafantasy'];
 
 			const addedTierSet: SearchRow[] = [['header', 'Gen9fantasy specific Pokemon']];
 			for (const pokemon in window.Gen9fantasydex) {
