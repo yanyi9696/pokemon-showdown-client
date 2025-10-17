b49a89e9a0dee2dcda65751aaab4c4de639351df
Anglos <2448172102@qq.com>
2025-10-17 14:58:24 +0800

    制作幻想mega水晶灯火灵

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -521,6 +521,10 @@ export const Dex = new class implements ModdedDex {
 			if (!data) data = {}; // 确保 data 对象存在，以防万一
 			data.spriteid = 'hawlucha';
 		}
+		if (id === 'chandeluremegafantasy') {
+			if (!data) data = {}; // 确保 data 对象存在，以防万一
+			data.spriteid = 'chandelure';
+		}
 		// ===== 我们添加的强制贴图修正代码 结束 =====
 
 			let species: Species;
@@ -1847,7 +1851,7 @@ const ModModifier: {
 		},
 		ModifyTierSet: (tierSet: SearchRow[], dex: ModdedDex, extra?: any): SearchRow[] => {
 			// 【新代码】创建一个“钉选”白名单
-			const pinnedPokemon = ['victreebelmegafantasy', 'hawluchamegafantasy'];
+			const pinnedPokemon = ['victreebelmegafantasy', 'hawluchamegafantasy', 'chandeluremegafantasy'];
 
 			const addedTierSet: SearchRow[] = [['header', 'Gen9fantasy specific Pokemon']];
 			for (const pokemon in window.Gen9fantasydex) {
