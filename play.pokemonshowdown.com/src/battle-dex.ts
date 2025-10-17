30db95d2d621df6cd0459f1ae76a34ba811252ab
Anglos <2448172102@qq.com>
2025-10-17 18:54:32 +0800

    制作幻想mega大竺葵

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -541,6 +541,10 @@ export const Dex = new class implements ModdedDex {
 			if (!data) data = {}; // 确保 data 对象存在，以防万一
 			data.spriteid = 'excadrill';
 		}
+		if (id === 'meganiummegafantasy') {
+			if (!data) data = {}; // 确保 data 对象存在，以防万一
+			data.spriteid = 'meganium';
+		}
 		// ===== 我们添加的强制贴图修正代码 结束 =====
 
 			let species: Species;
@@ -1869,7 +1873,7 @@ const ModModifier: {
 			// 【新代码】创建一个“钉选”白名单
 			const pinnedPokemon = [
 				'victreebelmegafantasy', 'hawluchamegafantasy', 'chandeluremegafantasy', 'froslassmegafantasy', 'delphoxmegafantasy', 
-				'dragalgemegafantasy', 'excadrillmegafantasy', 
+				'dragalgemegafantasy', 'excadrillmegafantasy', 'meganiummegafantasy', 
 			];
 
 			const addedTierSet: SearchRow[] = [['header', 'Gen9fantasy specific Pokemon']];
