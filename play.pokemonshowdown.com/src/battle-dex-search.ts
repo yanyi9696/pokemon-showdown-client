2d465b746eea9b4b4d45e6f75d9d29d28bbd8ddb
Anglos <2448172102@qq.com>
2026-02-10 13:16:00 +0800

    顶置大地神力

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1397,6 +1397,7 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 	}
 	private moveIsFantasy(id: ID, species: Dex.Species, moves: string[], set: Dex.PokemonSet | null) {
 		switch (id) {
+		case 'landswrath':
 		case 'bittermalice':
 		case 'mistyexplosion':
 		case 'meiguizhiwu':
