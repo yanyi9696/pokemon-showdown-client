d6b0e323bbcbf4cfc164fe65cc60cafd28629855
Anglos <2448172102@qq.com>
2026-04-16 02:42:12 +0800

    将cut加入顶置

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -1470,6 +1470,7 @@ class BattleMoveSearch extends BattleTypedSearch<'move'> {
 	}
 	private moveIsFantasy(id: ID, species: Dex.Species, moves: string[], set: Dex.PokemonSet | null) {
 		switch (id) {
+		case 'cut':
 		case 'psystrike':
 		case 'fanjibingjia':
 		case 'landswrath':
