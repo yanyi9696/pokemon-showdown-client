f621bc70e4cf461557949301e76b91288a6ba3b5
Anglos <2448172102@qq.com>
2026-05-01 02:18:01 +0800

    为onixfantasy添加自定义图标支持，并优化获取宝可梦图标的逻辑

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -1202,6 +1202,9 @@ export const Dex = new class implements ModdedDex {
 		if (finalId === 'urshifurapidstrikegmegafantasy') {
 			lookupId = 'urshifurapidstrikegmax' as ID;
 		}
+		if (finalId === 'onixfantasy') {
+			return `background:transparent url(${Dex.resourcePrefix}sprites/custom/onix-fantasy-icon.png) no-repeat scroll 0 0`;
+		}
 
 		if (lookupId.endsWith('fantasy')) {
 			lookupId = lookupId.slice(0, -8) as ID;
@@ -1249,7 +1252,7 @@ export const Dex = new class implements ModdedDex {
 			}
 		}
 
-		if (spriteid.endsWith('-fantasy')) {
+		if (id !== 'onixfantasy' && spriteid.endsWith('-fantasy')) {
 			spriteid = spriteid.slice(0, -8);
 		}
 
