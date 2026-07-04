ebf63783e4ed933d47852838c31a932c986d1610
Anglos <2448172102@qq.com>
2026-07-04 19:04:44 +0800

    调整宝可梦 Y 轴居中位置至 20%，更新特判字典以改善显示效果

--- play.pokemonshowdown.com/src/battle-dex.ts
+++ play.pokemonshowdown.com/src/battle-dex.ts
@@ -1482,7 +1482,7 @@ export const Dex = new class implements ModdedDex {
             const bgSize = "background-size: 95px 95px; ";
             
             // 1. 设置绝大部分宝可梦默认的 Y 轴居中位置
-            let finalY = "30%"; 
+            let finalY = "20%"; 
             
             // 2. 提取出干净的宝可梦 ID（去掉 -fantasy 后缀，方便字典统一判定）
             let baseId = data.spriteid;
@@ -1493,8 +1493,8 @@ export const Dex = new class implements ModdedDex {
             // 3. 【特判字典】：在这里把所有“太靠上”的宝可梦揪出来单独调教
             // 填入宝可梦的纯小写 ID，赋给它们一个更小的值（越小越往上抬）
             const customOffsets: { [id: string]: string } = {
-				'metagross': '5%',    // 巨金怪 往上提
-                'excadrill': '5%',    // 龙头地鼠 往上提
+				'metagross': '0%',    // 巨金怪 往上提
+                'excadrill': '0%',    // 龙头地鼠 往上提
                 'corviknight': '40%',  // 钢铠鸦 往下压
             };
 
